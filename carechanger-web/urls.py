ff90ab68fc5a9d0e69fe0d2815d699862e0f3468
Ross Clifford <Ross@Rosss-MBP.hitronhub.home>
2019-03-25 20:34:13 -0700

    Removed /accounts/ prefix for login page

--- carechanger-web/urls.py
+++ carechanger-web/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("addpatient/", sensors.views.add_patient, name="addpatient"),
     path("accounts/", include('accounts.urls')),
-    path("accounts/", include('django.contrib.auth.urls')),
+    path("", include('django.contrib.auth.urls')),
     path("signup/", sensors.views.signup, name='signup'),
 ]
