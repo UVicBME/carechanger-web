d552879062257c989d59e5ddd236c1d7c969e972
Ross Clifford <Ross@Rosss-MacBook-Pro.local>
2019-05-22 19:11:28 -0700

    Removed superuser only access for data form

--- sensors/models.py
+++ sensors/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+2from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.db.models.signals import post_save
