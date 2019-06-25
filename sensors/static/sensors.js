d1fd71ce00703524bb5920623162f7ae5bab3a89
Kelvin Filyk <kelvinfilyk@gmail.com>
2019-06-24 18:15:01 -0700

    Added chart, I think

--- sensors/static/sensors.js
+++ sensors/static/sensors.js
@@ -84,29 +84,29 @@ $(function(){
 $(function() {
 	var ctx = document.getElementById('patient_graph').getContext('2d');
 	var myChart = new Chart(ctx, {
-	    type: 'bar',
+	    type: 'line',
 	    data: {
-	        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+	        labels: ['7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM'],
 	        datasets: [{
-	            label: '# of Votes',
-	            data: [12, 19, 3, 5, 2, 3],
+	            label: 'Temperature',
+	            data: [3, 3, 4, 3, 3, 2, 2, 3, 3, 10],
 	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
+	                'rgba(255, 255, 255, 0)',
 	            ],
 	            borderColor: [
-	                'rgba(255, 99, 132, 1)',
-	                'rgba(54, 162, 235, 1)',
-	                'rgba(255, 206, 86, 1)',
-	                'rgba(75, 192, 192, 1)',
-	                'rgba(153, 102, 255, 1)',
-	                'rgba(255, 159, 64, 1)'
+	                'rgba(200, 50, 50, 1)',
 	            ],
-	            borderWidth: 1
+	            borderWidth: 3
+	        }, {
+	            label: 'Humidity',
+	            data: [1, 1, 2, 1, 1, 1, 1, 1, 2, 14],
+							backgroundColor: [
+	                'rgba(255, 255, 255, 0)',
+	            ],
+	            borderColor: [
+	                'rgba(50, 50, 200, 1)',
+	            ],
+	            borderWidth: 3
 	        }]
 	    },
 	    options: {
